aa859e7c5e46c0672f226808597e247ceb5270e9
amanape <83104063+amanape@users.noreply.github.com>
2024-06-22 18:57:24 +0300

    Export ObservationFactory

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Topic } from "./types/root";
 import Agent from "@abc/agent";
 import Runtime from "@abc/runtime";
 import TerminalManager from "@abc/terminal-manager";
+import ObservationFactory from "@utils/observation-factory";
 
-export { RxPubSub, Agent, Runtime, TerminalManager, Topic };
+export { RxPubSub, Agent, Runtime, TerminalManager, Topic, ObservationFactory };
